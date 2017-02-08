d67e8eff26e8636977dd45b1920ee8b1934b5177
Yamada Yohei <yamadayohei@gmail.com>
2017-02-08 23:05:25 +0900

    empty test scenario

--- mytest.c
+++ mytest.c
@@ -1,8 +1,25 @@
 #ifdef UNIT_TEST
+#include <stdio.h>
 #include "nu_unit.h"
-#endif
+int nu_tests_run, nu_tests_ok;
+
+static char*
+t01()
+{
+   return NULL;
+}
+
+int
+main(void)
+{
+   nu_run_test(t01);
+
+   return !!nu_result();
+}
+#else
 int
 main(void)
 {
    return 0;
 }
+#endif
